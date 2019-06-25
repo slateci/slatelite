6e12cafe814141cc030b462a0ff1adaa0543bb5a
Mitchell <mitchell@Mitchells-MacBook-Pro.local>
2019-06-25 16:53:58 -0600

    extend to join the cluster and install cvmfs

--- slate/init.sh
+++ slate/init.sh
@@ -7,9 +7,13 @@ mkdir -p -m 0700 "$HOME/.slate"
 echo $TOKEN > "$HOME/.slate/token"
 chmod 600 "$HOME/.slate/token"
 echo $ENDPOINT > "$HOME/.slate/endpoint"
+
 echo "Joining Slate Cluster..."
+
 slate cluster create $CLUSTERNAME --group $CLUSTERGROUP --org dev -y
+
 echo "Deploying squid proxy instance..."
+
 cat << EOF > squidconfig
 # Instance to label use case of Frontier Squid deployment
 # Generates app name as "osg-frontier-squid-[Instance]"
@@ -59,15 +63,25 @@ SquidConf:
   # within kubernetes clusters. 
 IPRange: 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16
 EOF
+
 slate app install osg-frontier-squid --cluster $CLUSTERNAME --group $GROUPNAME --conf squidconfig
+
 rm -rf squidconfig
+
 export CLUSTER_IP=$(kubectl get --namespace slate-group-$INITIALGROUP -o jsonpath="{.spec.clusterIP}" service osg-frontier-squid-cvmfs)
+
 echo "Adding CVMFS..."
+
 kubectl create namespace cvmfs
+
 git clone https://github.com/Mansalu/prp-osg-cvmfs.git
+
 cd prp-osg-cvmfs
+
 git checkout slate
+
 cd k8s/cvmfs
+
 cat << EOF > default.local 
 CVMFS_SERVER_URL="http://cvmfs-s1bnl.opensciencegrid.org:8000/cvmfs/@fqrn@;http://cvmfs-s1fnal.opensciencegrid.org:8000/cvmfs/@fqrn@;http://cvmfs-s1goc.opensciencegrid.org:8000/cvmfs/@fqrn@"
 CVMFS_KEYS_DIR=/etc/cvmfs/keys/opensciencegrid.org/
@@ -76,7 +90,11 @@ CVMFS_HTTP_PROXY="http://$CLUSTER_IP:3128"
 CVMFS_QUOTA_LIMIT=5000
 CVMFS_REPOSITORIES=atlas.cern.ch,atlas-condb.cern.ch,atlas-nightlies.cern.ch,sft.cern.ch,geant4.cern.ch,grid.cern.ch,cms.cern.ch,oasis.opensciencegrid.org
 EOF
+
 kubectl create configmap cvmfs-osg-config -n cvmfs --from-file=default.local
+
 kubectl create -f  accounts/
+
 kubectl create -f csi-processes/
+
 kubectl create -f storageclasses/
